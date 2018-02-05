f215ae12f4840ac99780812cb7edc9149668c35d
zverev <granatpost@inbox.ru>
2018-02-05 20:55:26 +0400

    homework 0502

--- ConsoleCalculator/Program.cs
+++ ConsoleCalculator/Program.cs
@@ -8,8 +8,68 @@ namespace ConsoleCalculator
 {
     class Program
     {
+
         static void Main(string[] args)
         {
+
+            Console.WriteLine("Калькулятор");
+
+
+            if (args.Length == 0)
+            {
+                Console.WriteLine("Калькулятор запущен без входных параметров. Задайте oper:");
+                var oper = Console.ReadLine();
+                Console.WriteLine("Введите первый операнд (а): ");
+                double a = Convert.ToDouble(Console.ReadLine());
+                Console.WriteLine("Введите второй операнд (b): ");
+                var b = Convert.ToDouble(Console.ReadLine());
+                var result=Calc.MyResult(a, b, oper);
+                Console.WriteLine($"Результат {oper} ({a};{b}) = {result}");
+                Console.ReadKey();
+            }
+
+            else
+
+            {
+                var oper = args[0];
+                var a = Convert.ToDouble(args[1]);
+                var b = Convert.ToDouble(args[2]);
+                var result = Calc.MyResult(a, b, oper);
+                //Console.WriteLine($"Результат {oper} ({a};{b}) = {result}");
+                Console.ReadKey();
+            }
         }
     }
+    public class Calc
+    {
+        private static double result;
+
+        public static double MyResult(double a, double b, string oper)
+        {
+            switch (oper)
+            {
+                case "Sub":
+                    result = a - b;
+                    Console.WriteLine($"Результат {oper}({a},{b}) = {result}");
+                    break;
+                case "Sum":
+                    result = a + b;
+                    Console.WriteLine($"Результат {oper}({a},{b}) = {result}");
+                    break;
+                case "Div":
+                    result = a / b;
+                    Console.WriteLine($"Результат {oper}({a},{b}) = {result}");
+                    break;
+                case "Mult":
+                    result = a*b;
+                    Console.WriteLine($"Результат {oper}({a},{b}) = {result}");
+                    break;
+
+            }
+            return result;
+        }
+
+    };
+
+
 }
